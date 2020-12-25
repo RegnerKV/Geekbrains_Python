d692cca5dde366c9118f65ca1dbf8f0ac2011146
Karina <dobrokv@gmail.com>
2020-12-25 18:58:23 +0700

    Задания к уроку 2 курса по Python.

---
+++ Lesson-2/Task-1.py
@@ -0,0 +1,10 @@
+my_int = 5
+my_float = 1.2
+my_str = "Привет"
+my_tuple = ('def', '456')
+my_list = ['abc', '123']
+my_dict = {'city': 'Moscow', 'country': 'Russia'}
+
+super_list = [my_int, my_float, my_str, my_list, my_tuple, my_dict]
+for i in super_list:
+    print(f'{i} is {type(i)}')
