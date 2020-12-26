2584cfb6d22484fc25875a18ab25ff8b3d0e7484
Karina <dobrokv@gmail.com>
2020-12-26 17:55:20 +0700

    Задания к уроку 2 курса по Python.

--- Lesson-2/Task-2.py
+++ Lesson-2/Task-2.py
@@ -1,16 +1,7 @@
-my_list = ['a', 'b', 'c', 'd', 'e']
-if len(my_list) % 2 == 0:
-    i = 0
-    while i < len(my_list):
-        el = my_list[i]
-        my_list[i] = my_list[i+1]
-        my_list[i+1] = el
-        i += 2
-else:
-    i = 0
-    while i < len(my_list) - 1:
-        el = my_list[i]
-        my_list[i] = my_list[i + 1]
-        my_list[i + 1] = el
-        i += 2
-print(my_list)
+a = int(input("Введите число: "))
+b = int(input("Введите число: "))
+c = int(input("Введите число: "))
+d = int(input("Введите число: "))
+e = int(input("Введите число: "))
+a, b, c, d, e = b, a, d, c, e
+print(a, b, c, d, e)
